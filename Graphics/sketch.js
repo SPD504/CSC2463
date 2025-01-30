b29e716529322fb2edec2eb0903c4af12d2f4eb9
SPD504 <157863365+SPD504@users.noreply.github.com>
2025-01-29 21:46:49 -0600

    NEWCOMMIT

--- Graphics/sketch.js
+++ Graphics/sketch.js
@@ -3,26 +3,7 @@ function setup() {
 }
 
 function draw() {
-  background('black');
-
-  fill(255, 255, 0);
-  noStroke();
-  circle(200, 200, 200);
-
-  fill(0);
-  triangle(200, 200, 100, 140, 100, 260);
-
-  fill(255, 0, 0);
-  noStroke();
-  arc(440, 150, 180, 100, 90, 0);
-  rect(350, 150, 180, 140);
-
-  fill(255);
-  circle(400, 180, 40);
-  circle(485, 180, 40);
-
-  fill(0, 0, 255);
-  circle(400, 180, 25);
-  circle(485, 180, 25);
-
+  background('lime');
+  circle(155, 200, 240);
+  square(315, 80, 240);
 }
